73d2935b6e5be54bf16f9f84f8e720babbd8c5fd
Fayjullah Haque Emon <fayjullah.h.emon@gmail.com>
2026-09-19 09:13:23 +0600

    update scripts.js

--- scripts/github-stats.js
+++ scripts/github-stats.js
@@ -1,1 +1,17 @@
 
+const username = "fayjullahhemon-2025";
+
+async function getGithubData() {
+    const response = await fetch(
+        `https://api.github.com/users/${username}`
+    );
+
+    const data = await response.json();
+
+    console.log("Name:", data.name);
+    console.log("Username:", data.login);
+    console.log("Followers:", data.followers);
+    console.log("Public Repositories:", data.public_repos);
+}
+
+getGithubData();
